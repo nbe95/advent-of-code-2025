f3c73b94b66e5555699916b1c495ab8b06730039
Niklas Bettgen <niklas@bettgen.de>
2025-12-08 16:54:19 +0100

    Day 08

---
+++ 08/box.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Box:
+    x: int
+    y: int
+    z: int
+
+    @classmethod
+    def from_string(cls, string: str) -> "Box":
+        x, y, z = string.split(",")
+        return cls(int(x), int(y), int(z))
+
+    def calc_sq_distance(self, other: "Box") -> float:
+        # Squared distance is sufficient for comparison/ordering
+        return (self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2
+
+
+def sort_by_distance(boxes: list[Box]) -> list[tuple[Box, Box]]:
+    """Compare each box against each other and sort results by Euclidean distance"""
+    combinations: list[tuple[Box, Box]] = []
+    for index in range(len(boxes)):
+        combinations.extend((boxes[index], boxes[cmp]) for cmp in range(index + 1, len(boxes)))
+    return sorted(combinations, key=lambda c: c[0].calc_sq_distance(c[1]))
+
+
+def _merge_circuits(circuits: list[list[Box]], box1: Box, box2: Box) -> list[list[Box]]:
+    """Merge two circuits including box1 and box2"""
+    merged: list[Box] = []
+    non_affected: list[list[Box]] = []
+
+    for circuit in circuits:
+        # Remove any existing circuits with the next to boxes to merge
+        if box1 in circuit or box2 in circuit:
+            merged.extend(circuit)
+        else:
+            non_affected.append(circuit)
+    return non_affected + [merged]
+
+
+def make_circuits(boxes: list[Box], num_to_connect: int) -> list[list[Box]]:
+    """Make groups of circuits (descending in size) by connecting the closest n boxes"""
+
+    # Start with each box in an individual circuit
+    circuits: list[list[Box]] = [[box] for box in boxes]
+    boxes_by_dist: list[tuple[Box, Box]] = sort_by_distance(boxes)
+
+    for _ in range(num_to_connect):
+        box1, box2 = boxes_by_dist.pop(0)
+        circuits = _merge_circuits(circuits, box1, box2)
+
+    return sorted(circuits, key=lambda c: len(c), reverse=True)
+
+
+def make_one_circuit(boxes: list[Box]) -> tuple[Box, Box]:
+    """Make one single circuit and return the last two boxes which need to be connected"""
+
+    # Start with each box in an individual circuit
+    circuits: list[list[Box]] = [[box] for box in boxes]
+    boxes_by_dist: list[tuple[Box, Box]] = sort_by_distance(boxes)
+
+    box1 = box2 = None
+    while len(circuits) > 1:
+        box1, box2 = boxes_by_dist.pop(0)
+        circuits = _merge_circuits(circuits, box1, box2)
+
+    return box1, box2
---
+++ 08/main.py
@@ -0,0 +1,32 @@
+"""Advent of Code - 2025-12-08"""
+
+import sys
+from functools import reduce
+from operator import mul
+
+from box import Box, make_circuits, make_one_circuit
+
+
+def main() -> int:
+    with open(sys.path[0] + "/input.txt", "r") as handle:
+
+        boxes = [Box.from_string(line.strip()) for line in handle.readlines()]
+        print(f"Total number of junction boxes: {len(boxes)}")
+
+        circuits = make_circuits(boxes, 1000)
+        print(
+            f"Number of circuits after connecting the closest 1000 junction boxes: {len(circuits)}"
+        )
+
+        result = reduce(mul, (len(c) for c in circuits[0:3]))
+        print(f"Product of the three largest circuits' sizes: {result}")
+
+        last_boxes = make_one_circuit(boxes)
+        product: int = reduce(mul, (box.x for box in last_boxes))
+        print(
+            f"Product of x coordinates of the last 2 boxes needed to form one single circuit: {product}"
+        )
+
+
+if __name__ == "__main__":
+    main()
